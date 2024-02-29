359eb3e4d353d653804715f82cfaf410bf3705e5
Vrinda Thakur <vrinda123thakur@gmail.com>
2024-02-29 17:21:54 +0530

    drawer changes

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,22 +1,139 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:uipages/pages/home_page1.dart';
+import 'package:uipages/pages/lab_reports.dart';
+import 'package:uipages/pages/manage_medications.dart';
+import 'package:uipages/pages/profile_page.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
+
+  //sign user out method
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.background,
-      child: Column(children: [
-        //Drawer Title
+      child:  Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Column(
+          children: [
+            //Drawer header
+        const DrawerHeader(
+          child: Icon(Icons.star_border_purple500_sharp, size: 50,color: Colors.deepPurple,)
+          ),
+
+        // Home
+        const SizedBox(height: 25,),
+        Padding(
+          padding: const EdgeInsets.only(left: 25.0),
+          child: ListTile(
+            leading: const Icon(Icons.home, size:40),
+            title: const Text(
+            'H O M E',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                            return HomePagee();
+                          }));
+            },
+          ),
+        ),
+
+        //Users tile
+        const SizedBox(height: 25,),
+        Padding(
+          padding: const EdgeInsets.only(left: 25.0),
+          child: ListTile(
+            leading: const Icon(Icons.person, size:40),
+            title: const Text(
+            'P R O F I L E',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                            return ProfilePage();
+                          }));
+            },
+          ),
+        ),
+
 
         //Manage Medications
-
+        const SizedBox(height: 25,),
+        Padding(
+          padding: const EdgeInsets.only(left: 25.0),
+          child: ListTile(
+            leading: const Icon(Icons.medication, size:40),
+            title: const Text(
+            'M A N A G E \nM E D I C A T I O N S',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                            return ManageMedication();
+                          }));
+            },
+          ),
+        ),
         //Lab Reports
+        const SizedBox(height: 10,),
+        Padding(
+          padding: const EdgeInsets.only(left: 25.0),
+          child: ListTile(
+            leading: const Icon(Icons.notes, size:40),
+            title: const Text(
+            'L A B   R E P O R T S',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                            return LabReport();
+                          }));
+            },
+          ),
+        ),
+        ],
+        ),
 
         //Log out
+        const SizedBox(height: 25,),
+        Padding(
+          padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+          child: ListTile(
+            leading: const Icon(Icons.medication, size:40),
+            title: const Text(
+            'L O G O U T',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+            ),
+            onTap: () {
+              Navigator.pop(context);
 
-        
+              //logout
+              signUserOut();
+            },
+          ),
+        ),
       ],)
     );
   }
--- lib/pages/home_page1.dart
+++ lib/pages/home_page1.dart
@@ -44,220 +44,216 @@ class _HomePageeState extends State<HomePagee> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      appBar: AppBar(backgroundColor: Colors.deepPurple[100],
-      title: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25.0),
-        child: Row(
-          children: [
-            SizedBox(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              
-                              const Text('Hello',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold, fontSize: 18)),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                user!.email!,
-                                style: const TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.bold),
-                              ),
-                            ],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[100],
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            children: [
+              SizedBox(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Hello',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18)),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      user!.email!,
+                      style: const TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+        
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      actions: [
-          IconButton(onPressed: signUserOut, icon: const Icon(Icons.logout))
-        ],
       ),
       drawer: MyDrawer(),
-      
-      body:  SafeArea(
+      body: SafeArea(
         child: SizedBox(
           child: Column(
-              children: [
-               
-                      //profile picture
-                      //Container(
-                          //padding: const EdgeInsets.all(12),
-                          //decoration: BoxDecoration(
-                            //color: const Color(0xffdcc7ff),
-                            //borderRadius: BorderRadius.circular(12),
-                          //),
-                          //child: const Icon(Icons.person)),
-                    //],
-                  //),
-                //),
-          
-                const SizedBox(
-                  height: 25,
-                ),
-                //card-> how do you feel?
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        color: Colors.pink[100],
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Row(
-                      children: [
-                        //animation or picture
-                        Container(
-                            height: 100,
-                            width: 100,
-                            child: Image.asset('lib/icons/drugs.png')),
-          
-                        SizedBox(width: 25),
-                        //how do yo feel + start button
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'How do you feel?',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 12,
-                              ),
-                              Text(
-                                'Fill your medical card right now!',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 12,
-                              ),
-                              Container(
-                                padding: EdgeInsets.all(12),
-                                decoration: BoxDecoration(
-                                    color: Colors.deepPurple[300],
-                                    borderRadius: BorderRadius.circular(
-                                      12,
-                                    )),
-                                child: Center(
-                                  child: Text('Get Started'),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-          
-                SizedBox(
-                  height: 25,
-                ),
-          
-                //search bar
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    padding: EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple[100],
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.search),
-                        border: InputBorder.none,
-                        hintText: 'How can we help you?',
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                //horizontal listview -> categories: dentist, surgeon etc.
-                Container(
-                  height: 80,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      CategoryCard(
-                        categoryName: 'Dentist',
-                        iconImagePath: 'lib/icons/tooth.png',
-                      ),
-                      CategoryCard(
-                        categoryName: 'Surgeon',
-                        iconImagePath: 'lib/icons/doctor.png',
-                      ),
-                      CategoryCard(
-                        categoryName: 'Cardiologist',
-                        iconImagePath: 'lib/icons/heart.png',
-                      ),
-                    ],
-                  ),
-                ),
-          
-                SizedBox(
-                  height: 25,
-                ),
-                //doctor list
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            children: [
+              //profile picture
+              //Container(
+              //padding: const EdgeInsets.all(12),
+              //decoration: BoxDecoration(
+              //color: const Color(0xffdcc7ff),
+              //borderRadius: BorderRadius.circular(12),
+              //),
+              //child: const Icon(Icons.person)),
+              //],
+              //),
+              //),
+
+              const SizedBox(
+                height: 25,
+              ),
+              //card-> how do you feel?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.pink[100],
+                      borderRadius: BorderRadius.circular(12)),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Doctor list',
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                      Text(
-                        'See all',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            color: Colors.grey[500]),
+                      //animation or picture
+                      Container(
+                          height: 100,
+                          width: 100,
+                          child: Image.asset('lib/icons/drugs.png')),
+
+                      SizedBox(width: 25),
+                      //how do yo feel + start button
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'How do you feel?',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            Text(
+                              'Fill your medical card right now!',
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                  color: Colors.deepPurple[300],
+                                  borderRadius: BorderRadius.circular(
+                                    12,
+                                  )),
+                              child: Center(
+                                child: Text('Get Started'),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-          
-                SizedBox(
-                  height: 20,
-                ),
-                Expanded(
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      DoctorCard(
-                        doctorImagePath: 'lib/image/doctor1.jpg',
-                        doctorName: 'Dr. Percy Jackson',
-                        doctorTitle: 'NeuroLogist, 7 y.e.',
-                      ),
-                      DoctorCard(
-                        doctorImagePath: 'lib/image/doctor2.jpg',
-                        doctorName: 'Dr. Anabeth Chase',
-                        doctorTitle: 'Pediatrician, 5 y.e.',
-                      ),
-                      DoctorCard(
-                        doctorImagePath: 'lib/image/doctor3.jpg',
-                        doctorName: 'Dr. Grover Underwood',
-                        doctorTitle: 'Orthopedist, 7 y.e.',
-                      ),
-                    ],
+              ),
+
+              SizedBox(
+                height: 25,
+              ),
+
+              //search bar
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      border: InputBorder.none,
+                      hintText: 'How can we help you?',
+                    ),
                   ),
                 ),
-                
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              //horizontal listview -> categories: dentist, surgeon etc.
+              Container(
+                height: 80,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    CategoryCard(
+                      categoryName: 'Dentist',
+                      iconImagePath: 'lib/icons/tooth.png',
+                    ),
+                    CategoryCard(
+                      categoryName: 'Surgeon',
+                      iconImagePath: 'lib/icons/doctor.png',
+                    ),
+                    CategoryCard(
+                      categoryName: 'Cardiologist',
+                      iconImagePath: 'lib/icons/heart.png',
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: 25,
+              ),
+              //doctor list
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Doctor list',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    Text(
+                      'See all',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.grey[500]),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    DoctorCard(
+                      doctorImagePath: 'lib/image/doctor1.jpg',
+                      doctorName: 'Dr. Percy Jackson',
+                      doctorTitle: 'NeuroLogist, 7 y.e.',
+                    ),
+                    DoctorCard(
+                      doctorImagePath: 'lib/image/doctor2.jpg',
+                      doctorName: 'Dr. Anabeth Chase',
+                      doctorTitle: 'Pediatrician, 5 y.e.',
+                    ),
+                    DoctorCard(
+                      doctorImagePath: 'lib/image/doctor3.jpg',
+                      doctorName: 'Dr. Grover Underwood',
+                      doctorTitle: 'Orthopedist, 7 y.e.',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      );
+    );
   }
 }
